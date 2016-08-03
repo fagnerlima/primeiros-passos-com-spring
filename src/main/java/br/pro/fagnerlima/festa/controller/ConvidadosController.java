e82726590457331e79cef9eb63668c99d4c57ade
Fagner Lima <contato@fagnerlima.pro.br>
2016-08-03 12:23:45 -0300

    Finalização do projeto

--- src/main/java/br/pro/fagnerlima/festa/controller/ConvidadosController.java
+++ src/main/java/br/pro/fagnerlima/festa/controller/ConvidadosController.java
@@ -3,22 +3,33 @@ package br.pro.fagnerlima.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.pro.fagnerlima.model.Convidado;
 import br.pro.fagnerlima.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 	
 	@Autowired
 	private Convidados convidados;
 	
-	@RequestMapping("/convidados")
+	@RequestMapping
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("ListaConvidados");
 		mv.addObject("convidados", convidados.todos());
+		mv.addObject(new Convidado());
 		
 		return mv;
+	}
+	
+	@RequestMapping(value = "/salvar", method = RequestMethod.POST)
+	public String salvar(Convidado convidado) {
+		this.convidados.adicionar(convidado);
+		
+		return "redirect:/convidados";
 	}
 	
 }
