b0c95bac27facbdb8b00352f30d29ae182f3667f
Fagner Lima <contato@fagnerlima.pro.br>
2016-08-03 10:07:39 -0300

    Primeiro commit

---
+++ src/main/java/br/pro/fagnerlima/festa/controller/ConvidadosController.java
@@ -0,0 +1,26 @@
+package br.pro.fagnerlima.festa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.pro.fagnerlima.model.Convidado;
+import br.pro.fagnerlima.repository.Convidados;
+
+@Controller
+public class ConvidadosController {
+	
+	@Autowired
+	private Convidados convidados;
+	
+	@RequestMapping("/convidados")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("ListaConvidados");
+		mv.addObject("convidados", convidados.todos());
+		mv.addObject(new Convidado());
+		
+		return mv;
+	}
+	
+}
---
+++ src/main/java/br/pro/fagnerlima/model/Convidado.java
@@ -0,0 +1,31 @@
+package br.pro.fagnerlima.model;
+
+public class Convidado {
+
+	private String nome;
+	private Integer quantidadeAcompanhantes;
+	
+	public Convidado() {}
+	
+	public Convidado(String nome, Integer quantidadeAcompanhantes) {
+		this.setNome(nome);
+		this.setQuantidadeAcompanhantes(quantidadeAcompanhantes);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+
+}
